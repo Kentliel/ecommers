ab5a7410fb5c2090b03ca18c7c8c30f91a76944d
Kent Carrero <kent.iluminati@gmail.com>
2026-04-24 23:35:09 +0200

    creacion de la entidad brand y category

---
+++ src/main/java/com/ecommers/models/Brand.java
@@ -0,0 +1,54 @@
+package com.ecommers.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "brands")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Brand
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+
+    @Column(length = 100)
+    private String country;
+
+    @Column(length = 255)
+    private String website;
+
+    @Column(nullable = false)
+    private Boolean active;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updateAt;
+
+    @PrePersist
+    public void prePersist()
+    {
+        this.createdAt = LocalDateTime.now();
+
+        if (this.active == null)
+            this.active = true;
+    }
+
+    @PreUpdate
+    public void preUpdate()
+    {
+        this.updateAt = LocalDateTime.now();
+    }
+
+}
